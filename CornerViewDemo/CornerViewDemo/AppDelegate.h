be80cdf46fd681cb9252e0448b9e2a9e0701603a
lemon <codebulucck@163.com>
2018-08-29 17:14:28 +0800

    init

---
+++ CornerViewDemo/CornerViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CornerViewDemo
+//
+//  Created by lemon on 2018/8/29.
+//  Copyright © 2018年 Lemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
